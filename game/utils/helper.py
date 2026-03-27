f4ec81b22969786d242810e2e2676094339ec64d
Brosky <75375494+BroskyGames@users.noreply.github.com>
2026-03-27 11:37:09 +0100

    Remove ReturnOptions Enum
    no longer needed, a clean-up

--- game/utils/helper.py
+++ game/utils/helper.py
@@ -4,12 +4,6 @@ from typing import Callable, Literal, TYPE_CHECKING, TypeVar, overload
 if TYPE_CHECKING:
     from ..logic.graph import RoomNode
 
-
-# class ReturnOptions(StrEnum):
-#     STORE_RESULTS = auto()
-#     CUMULATIVE = auto()
-#     IGNORE = auto()
-
 T = TypeVar('T')
 
 def bfs(start: "RoomNode", function: Callable[["RoomNode", T], T]) -> T:
